f89e3ac621778cc3626097c98f52ca5391a0f75f
Jacob W <Jwesolowski@gmail.com>
2020-10-15 23:42:57 -0500

    First commit

--- src/Tavunu.java
+++ src/Tavunu.java
@@ -7,8 +7,97 @@
  * pava -- items of status used for bargaining.
  *
  * @author Mithat Konar
- * @author Your Name
+ * @author Jacob Wesolowski 
  */
 public class Tavunu {
     /* See readme.md for what to do. */
+    
+    String tuvName;
+    int pava;
+    int yearOfBirth;
+    
+    Tavunu()
+    {
+        tuvName = null;
+        pava = 0;
+        yearOfBirth = 0;
+    }
+    
+    Tavunu(String name, int yob, int amount)
+    {
+        tuvName = name;
+        yearOfBirth = yob;
+        pava = amount;
+
+    }
+    
+    public boolean setName(String name)
+    {   
+        if (name.startsWith("D") || name.startsWith("T"))
+        {
+            tuvName = name;            
+            return true;
+ 
+        }
+        else
+        {
+            tuvName = null;
+            return false;
+        }
+    }
+    
+    public String getName()
+    {
+        return tuvName;
+        
+        
+    }
+    
+    public boolean spendPava(int amount)
+    {
+        if (amount < 0)
+        {
+            return false;
+        }
+        else
+        {
+            pava  = pava - amount;
+            return true;
+        } 
+    }
+    
+    public boolean receivePava(int amount)
+    {
+        if (amount < 0)
+        {
+            return false;
+        }
+        else
+        {
+            pava = pava + amount;
+            return true;
+        }
+
+    }
+    
+    public int getPava()
+    {
+        return pava;
+        
+    }
+    
+    public int getBirthYear()
+    {
+        return yearOfBirth;
+    }
+    
+    public void setBirthYear(int yob)
+    {
+        yearOfBirth = yob;
+    }
+    
+    public String toString()
+    {
+        return tuvName + " born in " + yearOfBirth + " has " + pava + " pava.";
+    }
 }
